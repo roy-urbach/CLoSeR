a4aed8da18dd035957cd41b0f1ce5688f7d81c9e
royur <roy.urbach@weizmann.ac.il>
2023-10-02 17:37:12 +0300

    - fixed a bug in io_utils.py

--- vision/utils/io_utils.py
+++ vision/utils/io_utils.py
@@ -5,5 +5,6 @@ import os
 def load_json(fn, base_path='config/'):
     if not fn.endswith('json'):
         fn = fn + '.json'
+    fn = os.path.join(base_path, fn)
     assert os.path.exists(fn), f"{fn} doesn't exist"
-    return json.loads(os.path.join(base_path, fn))
+    return json.loads(fn)
