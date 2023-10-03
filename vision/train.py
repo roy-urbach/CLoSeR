88be1e0b869b18a7e2d6b915a146aba02aef0808
royur <roy.urbach@weizmann.ac.il>
2023-10-03 13:42:39 +0300

    - now with stdout.flush()

--- vision/train.py
+++ vision/train.py
@@ -25,8 +25,11 @@ def run():
     time.sleep(3)
     print("DONE!")
 
-    with open(f"models/{model_name}/stupid_output.o", 'w') as f:
-        f.write(f"{model_name}\n{args.__dict__}\n{kwargs}")
+    import sys
+    sys.stdout.flush()
+
+    # with open(f"models/{model_name}/stupid_output.o", 'w') as f:
+    #     f.write(f"{model_name}\n{args.__dict__}\n{kwargs}")
 
     # TODO: uncomment to actually run
     # from utils.model import train
