f26d2b776a74ad2cb3853f678166df8546d2aaf6
royur <roy.urbach@weizmann.ac.il>
2023-11-12 15:59:08 +0200

    - added masked evaluation

--- vision/evaluate.py
+++ vision/evaluate.py
@@ -1,10 +1,23 @@
 from evaluation.ensemble import EnsembleVotingMethods
 from evaluation.evaluation import classify_head_eval_ensemble
 from evaluation.utils import save_evaluation_json, load_evaluation_json
-from utils.data import Cifar10
+from utils.data import Cifar10, Data
 from utils.io_utils import load_json
 from utils.tf_utils import load_model_from_json
 from utils.utils import get_class
+import numpy as np
+
+
+def get_masked_ds(model, dataset=Cifar10()):
+    if isinstance(model, str):
+        model = load_model_from_json(model)
+    patch_layer = model.get_layer(model.name + '_patch')
+    pathway_indices = model.get_layer(model.name + '_pathways').indices.numpy()
+    setup_func = lambda x: np.transpose(patch_layer(x).numpy()[:, pathway_indices - 1], [0, 1, 3, 2]).reshape(
+        x.shape[0], -1, pathway_indices.shape[-1])
+    ds = Data(setup_func(dataset.get_x_train()), dataset.get_y_train(),
+              setup_func(dataset.get_x_test()), dataset.get_y_test())
+    return ds
 
 
 def main():
@@ -61,6 +74,12 @@ def evaluate(model, knn=False, linear=True, ensemble=True, ensemble_knn=False, s
                                                    voting_methods=[EnsembleVotingMethods.ArgmaxMeanProb]))
         save_res()
 
+        if not any([k.startswith("image_pathway") for k in results.keys()]):
+            masked_ds = get_masked_ds(model, dataset=dataset)
+            results.update(classify_head_eval_ensemble(masked_ds, base_name='image_', svm=False,
+                                                       voting_methods=[EnsembleVotingMethods.ArgmaxMeanProb]))
+            save_res()
+
     if ensemble_knn:
         results.update(classify_head_eval_ensemble(embd_dataset, linear=False, svm=False, ensemble=True, k=15,
                                                    voting_methods=EnsembleVotingMethods))
@@ -70,6 +89,7 @@ def evaluate(model, knn=False, linear=True, ensemble=True, ensemble_knn=False, s
 
 if __name__ == '__main__':
     import run_before_script
+
     run_before_script.run()
 
     main()
