6f4d750fd9ed5c34b540d12cf0ea4e8caa1a1a20
royur <roy.urbach@weizmann.ac.il>
2023-12-03 11:11:16 +0200

    - added normzliation of data in Data (needs flag), and flattenning of labels (default = True)

--- vision/evaluation/evaluation.py
+++ vision/evaluation/evaluation.py
@@ -32,9 +32,9 @@ def classify_head_eval(dataset, m=lambda x: x.reshape(x.shape[0], -1), pca=False
     else:
         from sklearn.neighbors import KNeighborsClassifier
         model = KNeighborsClassifier(k)
-    model.fit(train_embd, y_train.flatten()[inds])
-    train_score = model.score(train_embd, y_train.flatten()[inds], **score_kwargs)
-    test_score = model.score(m(x_test), y_test.flatten(), **score_kwargs)
+    model.fit(train_embd, y_train[inds])
+    train_score = model.score(train_embd, y_train[inds], **score_kwargs)
+    test_score = model.score(m(x_test), y_test, **score_kwargs)
     print(
         f"Train acc: {train_score:.5f}; Test acc: {test_score:.5f}")
     return train_score, test_score
@@ -57,14 +57,14 @@ def classify_head_eval_ensemble(dataset, linear=True, k=10, base_name='',
 
     for i, pathway in enumerate(ensemble.models):
         from utils.data import Data
-        cur_ds = Data(x_train[..., i], y_train.flatten(), x_test[..., i], y_test.flatten())
+        cur_ds = Data(x_train[..., i], y_train, x_test[..., i], y_test)
         res[base_name + f'pathway{i}_{name}'] = (pathway.score(*cur_ds.get_train()),
                                                  pathway.score(*cur_ds.get_test()))
     res[base_name + f'pathways_mean_{name}'] = np.mean(list(res.values()), axis=0).tolist()
 
     for voting_method in voting_methods:
-        train_score = ensemble.score(x_train, y_train.flatten(), voting_method=voting_method)
-        test_score = ensemble.score(x_test, y_test.flatten(), voting_method=voting_method)
+        train_score = ensemble.score(x_train, y_train, voting_method=voting_method)
+        test_score = ensemble.score(x_test, y_test, voting_method=voting_method)
         res[base_name + f"ensemble_{name}_" + voting_method.name] = (train_score, test_score)
         printd(f"{base_name + voting_method.name} Train acc: {train_score:.5f}; Test acc: {test_score:.5f}")
     return res
