81d3b3e35b703b55656a52399c3f71671b77d45c
royur <roy.urbach@weizmann.ac.il>
2024-02-14 11:00:17 +0200

    - added fmnist to data.py

--- vision/utils/data.py
+++ vision/utils/data.py
@@ -66,3 +66,9 @@ class MNIST(Data):
     def __init__(self, *args, **kwargs):
         (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
         super().__init__(x_train[..., None], y_train, x_test[..., None],  y_test, *args, **kwargs)
+
+
+class FMNIST(Data):
+    def __init__(self, *args, **kwargs):
+        (x_train, y_train), (x_test, y_test) = tf.keras.datasets.fashion_mnist.load_data()
+        super().__init__(x_train[..., None], y_train, x_test[..., None],  y_test, *args, **kwargs)
