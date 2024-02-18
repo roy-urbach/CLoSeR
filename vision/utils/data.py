c9532a897666b39c401c83bc1b23cda0d4cecbf2
royur <roy.urbach@weizmann.ac.il>
2024-02-18 10:52:26 +0200

    - added cifar100

--- vision/utils/data.py
+++ vision/utils/data.py
@@ -62,6 +62,12 @@ class Cifar10(Data):
         super().__init__(x_train, y_train, x_test,  y_test, *args, **kwargs)
 
 
+class Cifar100(Data):
+    def __init__(self, *args, **kwargs):
+        (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar100.load_data()
+        super().__init__(x_train, y_train, x_test,  y_test, *args, **kwargs)
+
+
 class MNIST(Data):
     def __init__(self, *args, **kwargs):
         (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
