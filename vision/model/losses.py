9b4fcab62b213715b758a6492979ba3f0cc60058
royur <roy.urbach@weizmann.ac.il>
2024-04-03 16:41:26 +0300

    - fixed stable issue in map_rep_dev

--- vision/model/losses.py
+++ vision/model/losses.py
@@ -125,11 +125,15 @@ class GeneralPullPushGraphLoss(ContrastiveSoftmaxLoss):
 
     def map_rep_dev(self, exp_logits=None, logits=None):
         assert (logits is not None) or (exp_logits is not None)
-        self_sim = exp_logits if exp_logits is not None else self.calculate_exp_logits(None, logits)
-        b = tf.shape(self_sim)[0]
-        n = tf.shape(self_sim)[-1]
+        cur = exp_logits if exp_logits is not None else logits # Will be converted to exp_logits later
+        b = tf.shape(cur)[0]
+        n = tf.shape(cur)[-1]
         if self.remove_diag:
-            self_sim = tf.reshape(self_sim[~tf.eye(b, dtype=tf.bool)[..., None, None] & tf.eye(n, dtype=tf.bool)[None, None]], (b-1, b, n))
+            self_sim = tf.reshape(cur[~tf.eye(b, dtype=tf.bool)[..., None, None] & tf.eye(n, dtype=tf.bool)[None, None]], (b-1, b, n))
+        else:
+            self_sim = cur
+        if exp_logits is None:
+            self_sim = self.calculate_exp_logits(None, self_sim)
         map_rep = self_sim / tf.reduce_sum(self_sim, axis=0, keepdims=True)  # (b-1, b, n)
         log_map_rep = tf.experimental.numpy.log2(tf.maximum(map_rep, self.log_eps))
         cross_ent = tf.einsum('ibn,ibm->bnm', map_rep, log_map_rep)  # (b, n, n)
