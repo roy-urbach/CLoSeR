ff31d3a9eafae3d84e6cf35e5a46d6b6c2207e43
royur <roy.urbach@weizmann.ac.il>
2024-05-20 18:25:55 +0300

    - added measure.py for inter-pathway measures

---
+++ vision/measure.py
@@ -0,0 +1,61 @@
+from measures.utils import save_measures_json, measure_model, get_measuring_time, load_measures_json
+
+
+def main():
+    import argparse
+
+    def parse():
+        parser = argparse.ArgumentParser(description='Measure some metrics about the pathways of a model')
+        parser.add_argument('-j', '--json', type=str, help='name of the config json')
+        parser.add_argument('-b', '--batch', type=int, default=128, help='batch size')
+        parser.add_argument('-i', '--iterations', type=int, default=50, help='number of repeats')
+        parser.add_argument('--override', action=argparse.BooleanOptionalAction, default=False)
+        return parser.parse_args()
+
+    args = parse()
+    args.json = ".".join(args.json.split(".")[:-1]) if args.json.endswith(".json") else args.json
+    measuring_fn = f'models/{args.json}/is_evaluating'
+
+    import os
+    if os.path.exists(measuring_fn):
+        print("already measuring!")
+        return
+    else:
+        with open(measuring_fn, 'w') as f:
+            f.write("Yes!")
+
+    try:
+        res = measure(args.json, b=args.b, iterations=args.iterations, save_results=True, override=args.override)
+    finally:
+        os.remove(measuring_fn)
+    return res
+
+
+def measure(model, save_results=False, override=False, **kwargs):
+    model_name = model if isinstance(model, str) else model.name
+    if not override:
+        from utils.io_utils import get_output_time
+        output_time = get_output_time(model)
+        measuring_time = get_measuring_time(model)
+
+        if output_time and measuring_time and output_time < measuring_time:
+            print(f"Tried to measure, but output time is {output_time} and measuring time is {measuring_time} and override is False")
+            return load_measures_json(model_name)
+
+    results = load_measures_json(model_name) if not override else {}
+
+    if results is None:
+        results = {}
+    res_dct = measure_model(model, **kwargs)
+    if save_results:
+        save_measures_json(model_name, res_dct)
+
+    return results
+
+
+if __name__ == '__main__':
+    import run_before_script
+
+    run_before_script.run()
+
+    main()
---
+++ vision/measures/utils.py
@@ -0,0 +1,83 @@
+from model.model import load_model_from_json
+from utils.io_utils import load_json, save_json, get_file_time
+from utils.utils import get_class, counter
+import numpy as np
+from enum import Enum, auto
+
+MEASURES_FILE_NAME = 'measures'
+
+
+class CrossPathMeasures(Enum):
+    Acc = auto()
+    MaxLike = auto()
+    Entropy = auto()
+    MaxLikeNoSelf = auto()
+    EntropyNoSelf = auto()
+    Distance = auto()
+    Correlation = auto()
+    DKL = auto()
+
+
+def load_measures_json(model_name):
+    dct_with_list = load_json(MEASURES_FILE_NAME, base_path=f'models/{model_name}')
+    dct = {k: np.array(v) for k, v in dct_with_list.items()}
+    return dct
+
+
+def save_measures_json(model_name, dct):
+    dct = {k: v.tolist() if isinstance(v, np.ndarray) else v for k, v in dct.items()}
+    save_json(MEASURES_FILE_NAME, dct, base_path=f'models/{model_name}')
+
+
+def get_measuring_time(model_name, raw=True):
+    return get_file_time(f'models/{model_name}/{MEASURES_FILE_NAME}.json', raw=raw)
+
+
+def measure_model(model, iterations=50, b=128):
+    import tensorflow as tf
+
+    if isinstance(model, str):
+        model = load_model_from_json(model)
+
+    import utils.data
+    kwargs = load_json(model)
+    dataset = get_class(kwargs.get('dataset', 'Cifar10'), utils.data)(**kwargs.get("data_kwargs", {}))
+    # train_embd = model.predict(dataset.get_x_train())
+    test_embd = model.predict(dataset.get_x_test())
+
+    n = test_embd.shape[1]
+
+    loss = model.loss[model.name + "_embedding"]
+
+    res_dct = {k: [] for k in CrossPathMeasures}
+
+    triu_inds = np.triu_indices(b, 1)
+
+    for i in counter(range(iterations)):
+        cur_samples = test_embd[np.random.permutation(test_embd.shape[0])[:b]]
+        cur_dists = np.sqrt(loss.calculate_dists(cur_samples).numpy())
+        logits = loss.calculate_logits(None, dist=cur_dists)
+        mrdev = loss.map_rep_dev(exp_logits=None, logits=tf.linalg.diag_part(logits))   # (b, n)
+
+        likelihood = model.loss[model.name + "_embedding"].calculate_likelihood(logits=logits)
+        res_dct[CrossPathMeasures.Acc].append(tf.reduce_mean(tf.cast((likelihood >= tf.reduce_max(likelihood, axis=0, keepdims=True))[
+                                                     tf.eye(tf.shape(likelihood)[0], dtype=tf.bool)], dtype=tf.float32),
+                                         axis=0))
+        res_dct[CrossPathMeasures.MaxLike].append(tf.reduce_mean(likelihood[tf.eye(tf.shape(likelihood)[0], dtype=tf.bool)], axis=0))
+        res_dct[CrossPathMeasures.Entropy].append(-tf.reduce_mean(tf.einsum('bBnN,bBnN->BnN', likelihood, tf.math.log(likelihood)), axis=0))
+
+        likelihood_without_self = tf.reshape(
+            likelihood[tf.tile(~tf.eye(b, dtype=tf.bool)[..., None, None], [1, 1, n, n])],
+            (b - 1, b, n, n))
+        likelihood_without_self = likelihood_without_self / tf.reduce_sum(likelihood_without_self, axis=0,
+                                                                          keepdims=True)
+        res_dct[CrossPathMeasures.EntropyNoSelf].append(
+            -tf.reduce_mean(tf.einsum('bBnN,bBnN->BnN', likelihood_without_self, tf.math.log(likelihood_without_self)),
+                            axis=0))
+
+        res_dct[CrossPathMeasures.Distance].append(cur_dists[np.arange(b), np.arange(b)].mean(axis=0))
+        res_dct[CrossPathMeasures.Correlation].append(np.corrcoef(cur_dists[triu_inds][..., np.arange(n), np.arange()].T))
+        res_dct[CrossPathMeasures.DKL].append(tf.reduce_mean(mrdev, axis=0))
+
+    res_dct = {k.name: np.mean(v, axis=0) for k, v in res_dct}
+    return res_dct
