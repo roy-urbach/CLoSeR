fe4fdf73c419b13ad46b57c8ae4accafd43b6e11
royur <roy.urbach@weizmann.ac.il>
2024-05-20 18:50:04 +0300

    - fixed a missing n in arange

--- vision/measures/utils.py
+++ vision/measures/utils.py
@@ -79,7 +79,7 @@ def measure_model(model, iterations=50, b=128):
                             axis=0))
 
         res_dct[CrossPathMeasures.Distance].append(cur_dists[np.arange(b), np.arange(b)].mean(axis=0))
-        res_dct[CrossPathMeasures.Correlation].append(np.corrcoef(cur_dists[triu_inds][..., np.arange(n), np.arange()].T))
+        res_dct[CrossPathMeasures.Correlation].append(np.corrcoef(cur_dists[triu_inds][..., np.arange(n), np.arange(n)].T))
         res_dct[CrossPathMeasures.DKL].append(tf.reduce_mean(mrdev, axis=0))
 
     res_dct = {k.name: np.mean(v, axis=0) for k, v in res_dct}
