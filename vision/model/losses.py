faffb008bc1c0365474709a8ff33ac4a243533f8
royur <roy.urbach@weizmann.ac.il>
2024-05-26 13:44:07 +0300

    - added MeanConfidence

--- vision/model/losses.py
+++ vision/model/losses.py
@@ -505,3 +505,20 @@ class ConfidenceContrastiveLoss(ContrastiveSoftmaxLoss):
         else:
             loss = loss - self.c_w * tf.reduce_mean(confidence)
         return loss
+
+
+class MeanConfidence(tf.keras.Metric.Metric):
+    def __init__(self, name='mean_confidence', alpha=0.75, **kwargs):
+        super(MeanConfidence, self).__init__(name=name, **kwargs)
+        self.mean_conf = self.add_variable(
+            shape=(),
+            initializer='zeros',
+            name='mean_conf'
+        )
+        self.alpha = alpha
+
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        self.mean_conf.assign(self.alpha * self.mean_conf + (1-self.alpha) * tf.reduce_mean(tf.sigmoid(y_pred[:, -1])))
+
+    def result(self):
+        return self.mean_conf
