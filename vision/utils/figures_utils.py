625618753dfed0ab4b7eb7456a792392fec4361d
royur <roy.urbach@weizmann.ac.il>
2024-05-27 18:22:58 +0300

    - added mask option to compare_measures

--- vision/utils/figures_utils.py
+++ vision/utils/figures_utils.py
@@ -604,7 +604,7 @@ def plot_measures(model_regex, mask=None, save=False):
             savefig(f"figures/{measure}")
 
 
-def compare_measures(*models, names=None, log=False, **kwargs):
+def compare_measures(*models, names=None, log=False, mask=None, **kwargs):
     from measures.utils import CrossPathMeasures, load_measures_json
     if names is None:
         names = models
@@ -612,8 +612,8 @@ def compare_measures(*models, names=None, log=False, **kwargs):
     for i, k in enumerate(CrossPathMeasures):
         plt.subplot(3, 3, i+1)
         plt.title("log "*log +  k.name)
-        remove_diag = lambda arr: np.where(np.eye(len(arr)) > 0, np.nan, arr)
+        remove = lambda arr: np.where(np.eye(len(arr)) > 0, np.nan, arr) if mask is None else np.where(mask, np.nan, arr)
         f_log = lambda arr: np.log(arr) if log else arr
-        dct_to_multiviolin({name: f_log(remove_diag(load_measures_json(model)[k.name]))
+        dct_to_multiviolin({name: f_log(remove(load_measures_json(model)[k.name]))
                             for name, model in zip(names, models)}, fig=fig, **kwargs)
     plt.tight_layout()
