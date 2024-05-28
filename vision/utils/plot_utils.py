93108e9a9bc5d30dfa1b7845c8ccf71b458e1e6d
royur <roy.urbach@weizmann.ac.il>
2024-05-28 12:09:15 +0300

    - added violinplot kwargs option

--- vision/utils/plot_utils.py
+++ vision/utils/plot_utils.py
@@ -47,8 +47,8 @@ def savefig(fn):
     print(f"saved figure as {fn}")
 
 
-def violinplot_with_CI(arr, x, c='C0', width=0.5):
-    vi = plt.violinplot(arr, [x], showextrema=False, showmeans=False, width=width)
+def violinplot_with_CI(arr, x, c='C0', widths=0.5):
+    vi = plt.violinplot(arr, [x], showextrema=False, showmeans=False, widths=widths)
     for pc in vi['bodies']:
         pc.set_facecolor(c)
     mean = arr.mean()
@@ -58,13 +58,13 @@ def violinplot_with_CI(arr, x, c='C0', width=0.5):
     plt.errorbar([x], mean, yerr=CI, marker='o', capsize=10, c=c)
 
 
-def multiviolin(arr, xshift=0, xs=None, fig=None, width=0.5):
+def multiviolin(arr, xshift=0, xs=None, fig=None, **kwargs):
     if fig is None:
         fig = plt.figure()
     for i in range(len(arr)):
         if (isinstance(arr[i], np.ndarray) and arr[i].size) or (not isinstance(arr[i], np.ndarray) and arr):
             if np.isnan(arr[i]).all(): continue
-            violinplot_with_CI(arr[i][~np.isnan(arr[i])], [i + xshift] if xs is None else xs[i], c=f"C{i}", width=width)
+            violinplot_with_CI(arr[i][~np.isnan(arr[i])], [i + xshift] if xs is None else xs[i], c=f"C{i}", **kwargs)
 
 
 def dct_to_multiviolin(dct, rotation=0, xs=None, **kwargs):
