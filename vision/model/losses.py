17a1f76a76569fadeac87d4e363298b7f611c3c1
royur <roy.urbach@weizmann.ac.il>
2024-05-30 11:40:55 +0300

    - normalized LogLikelihoodIterativeSoftmax

--- vision/model/losses.py
+++ vision/model/losses.py
@@ -608,4 +608,4 @@ class LogLikelihoodIterativeSoftmax(Loss):
                         log_z = tf.math.reduce_logsumexp(logits, axis=side)
                         log_likelihood = diag_logits - log_z
                         loss = loss - tf.reduce_mean(log_likelihood)
-        return loss
+        return loss / (n ** 2 if self.a is None else tf.reduce_sum(self.a))
