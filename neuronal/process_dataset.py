179cda6cbdea6270f3c314212b4e74c8c1492c7b
royur <roy.urbach@weizmann.ac.il>
2024-08-12 14:45:48 +0300

    - added process_dataset.py

---
+++ neuronal/process_dataset.py
@@ -0,0 +1,85 @@
+import os
+import numpy as np
+
+
+def process_session(index):
+    from allensdk.brain_observatory.ecephys.ecephys_project_cache import EcephysProjectCache
+
+    output_dir = '../../../../jonatham/allen_institute_data/ecephys_cache_dir/'
+    assert f"session_{index}" in os.listdir(
+        output_dir), f"session {index} not in {output_dir}, instead {os.listdir(output_dir)}"
+
+    manifest_path = os.path.join(output_dir, "manifest.json")
+
+    cache = EcephysProjectCache.from_warehouse(manifest=manifest_path)
+
+    session = cache.get_session_data(index,
+                                     isi_violations_maximum=np.inf,
+                                     amplitude_cutoff_maximum=np.inf,
+                                     presence_ratio_minimum=-np.inf
+                                     )
+
+    session_dir = str(session.ecephys_session_id)
+
+    for name in ["natural_movie_one", "natural_movie_three"]:
+        stim = session.stimulus_presentations
+        movie_stim = stim[stim.stimulus_name == name]
+        starts = movie_stim.start_time[stim.frame == 0]
+        ends = movie_stim.stop_time[
+            movie_stim.frame == (np.where(movie_stim.frame.to_numpy() == 'null', 0, movie_stim.frame.to_numpy())).max()]
+        for repeat, (start, end) in enumerate(zip(starts, ends)):
+            spikes_mask = {k: (v >= start) & (v <= end) for k, v in session.spike_times.items()}
+            filtered_spike_times = {str(k): v[spikes_mask[k]] for k, v in session.spike_times.items()}
+            filtered_spike_amplitudes = {str(k): v[spikes_mask[k]] for k, v in session.spike_amplitudes.items() if
+                                         k in spikes_mask}
+            filtered_running_speed = session.running_speed[
+                ~ ((session.running_speed.start_time > end) | (start > session.running_speed.end_time))]
+            filtered_invalid_times = session.invalid_times[
+                ~ ((session.invalid_times.start_time > end) | (start > session.invalid_times.stop_time))]
+            path = [session_dir, name, str(repeat)]
+            os.makedirs(os.path.join(*path), exist_ok=True)
+            with open(os.path.join(*path, "spike_times.npz"), 'wb') as f:
+                np.savez(f, **filtered_spike_times)
+            print("saved spike times")
+
+            with open(os.path.join(*path, "spike_amplitudes.npz"), 'wb') as f:
+                np.savez(f, **filtered_spike_amplitudes)
+            print("saved spike amplitudes")
+
+            filtered_running_speed.to_csv(os.path.join(*path, "running_speed.csv"))
+            print("saved running speed")
+
+            filtered_invalid_times.to_csv(os.path.join(*path, "invalid_times.csv"))
+            print("saved invalid times")
+
+            stimulus_rows = (movie_stim.start_time >= start) & (movie_stim.start_time <= end)
+            frames_start = movie_stim.start_time[stimulus_rows].to_numpy()
+            frames_end = movie_stim.stop_time[stimulus_rows].to_numpy()
+
+            with open(os.path.join(*path, "frame_times.npz"), 'wb') as f:
+                np.savez(f, start=frames_start, end=frames_end)
+            print("printed frame times")
+
+    session.units.to_csv(os.path.join(session_dir, "units.csv"))
+    print("saved units")
+    session.probes.to_csv(os.path.join(session_dir, "probes.csv"))
+    print("saved probes")
+
+    import json
+    with open(os.path.join(session_dir, "metadata.json"), 'w') as f:
+        json.dump({k: v for k, v in session.metadata.items() if "start_time" not in k}, f, indent=4)
+    print("saved metadata")
+
+    with open(os.path.join(session_dir, "start_time.txt"), 'w') as f:
+        f.write(str(session.session_start_time))
+    print("saved start time")
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Process allen data')
+    parser.add_argument('--index', type=int, default=None, help='session index', required=True)
+
+    args, _ = parser.parse_known_args()
+    process_session(args.index)
