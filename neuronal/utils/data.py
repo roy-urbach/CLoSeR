c6f9acf14c22169bfaea860f0e0fbbe381378992
royur <roy.urbach@weizmann.ac.il>
2024-08-12 16:44:53 +0300

    - Trials load on demand - units and probes added to session

--- neuronal/utils/data.py
+++ neuronal/utils/data.py
@@ -18,6 +18,8 @@ class Session:
         self.metadata = None
         self.start_time = None
         self.trials = None
+        self.units = None
+        self.probes = None
         self._path = os.path.join(DATA_DIR, self.id)
         self._load()
 
@@ -36,6 +38,9 @@ class Session:
             self.trials[stimulus] = [Trial(self.id, stimulus, trial_num)
                                      for trial_num in sorted(os.listdir(os.path.join(self._path, stimulus)))]
 
+        self.units = pd.read_csv(os.path.join(self._path, "units.csv"))
+        self.probes = pd.read_csv(os.path.join(self._path, "probes.csv"))
+
     def get_trials(self, stimulus=None):
         if stimulus is not None:
             return self.trials.get(stimulus, [])[:]
@@ -47,31 +52,79 @@ class Session:
 
 
 class Trial:
-    def __init__(self, session_id, stimulus, trial_num):
+    def __init__(self, session_id, stimulus, trial_num, binsize=0.02):
         self._path = os.path.join(DATA_DIR, session_id, stimulus, trial_num)
         self.stimulus = stimulus
         self.session_id = session_id
         self.trial_num = trial_num
+        self.spike_bins = None
         self.spike_times = None
         self.spike_amplitudes = None
         self.running_speed = None
         self.invalid_times = None
         self.frame_start = None
         self.frame_end = None
-        self._load()
+        self.binsize = binsize
 
-    def _load(self):
-        self.spike_times = loadz(os.path.join(self._path, "spike_times.npz"))
-        self.spike_amplitudes = loadz(os.path.join(self._path, "spike_amplitudes.npz"))
-        self.running_speed = pd.read_csv(os.path.join(self._path, "running_speed.csv"))
-        self.invalid_times = pd.read_csv(os.path.join(self._path, "invalid_times.csv"))
-        frame_times = loadz(os.path.join(self._path, "frame_times.npz"))
-        self.frame_start = frame_times['start']
-        self.frame_end = frame_times['end']
+    def _load_spike_times(self):
+        if self.spike_times is None:
+            self.spike_times = loadz(os.path.join(self._path, "spike_times.npz"))
+
+    def _load_spike_amplitudes(self):
+        if self.spike_amplitudes is None:
+            self.spike_amplitudes = loadz(os.path.join(self._path, "spike_amplitudes.npz"))
+
+    def _load_running_speed(self):
+        if self.running_speed is None:
+            self.running_speed = pd.read_csv(os.path.join(self._path, "running_speed.csv"))
+
+    def _load_invalid_times(self):
+        if self.invalid_times is None:
+            self.invalid_times = pd.read_csv(os.path.join(self._path, "invalid_times.csv"))
+
+    def _load_frame_start(self):
+        if self.frame_start is None:
+            frame_times = loadz(os.path.join(self._path, "frame_times.npz"))
+            self.frame_start = frame_times['start']
+            self.frame_end = frame_times['end']
+
+    def _load_frame_end(self):
+        if self.frame_start is None:
+            frame_times = loadz(os.path.join(self._path, "frame_times.npz"))
+            self.frame_start = frame_times['start']
+            self.frame_end = frame_times['end']
+
+    def get_spike_times(self):
+        self._load_spike_times()
+        return self.spike_times
+
+    def get_spike_amplitudes(self):
+        self._load_spike_amplitudes()
+        return self.spike_amplitudes
+
+    def get_running_speed(self):
+        self._load_running_speed()
+        return self.running_speed
+
+    def get_invalid_times(self):
+        self._load_invalid_times()
+        return self.invalid_times
+
+    def get_frame_start(self):
+        self._load_frame_start()
+        return self.frame_start
+
+    def get_frame_end(self):
+        self._load_frame_end()
+        return self.frame_end
+
+    def _load_spike_bins(self):
+        pass
+        # if self.spike_bins is None:
+        #     if os.path.join()
+
+    def get_spike_bins(self):
+        pass
 
     def __repr__(self):
         return f"<Trial {self.trial_num} (session {self.session_id}, stim {self.stimulus})>"
-
-
-
-
