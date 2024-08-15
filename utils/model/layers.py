b0783ad2e9b9ff669a52a058fbcab18af950f4db
royur <roy.urbach@weizmann.ac.il>
2024-08-15 18:30:30 +0300

    - Made SplitPathways a general one

--- utils/model/layers.py
+++ utils/model/layers.py
@@ -296,3 +296,85 @@ class ConvNet:
             if self.pools is not None:
                 x = self.pools[i](x)
         return x
+
+
+@serialize
+class SplitPathways(tf.keras.layers.Layer):
+    def __init__(self, num_features, token_per_path=False, n=2, d=0.5, intersection=True, fixed=False,
+                 seed=0, class_token=True, pathway_to_cls=None, **kwargs):
+        super(SplitPathways, self).__init__(**kwargs)
+        assert intersection or (n*int(num_features * d)) <= num_features
+        self.n = n
+        self.seed = seed
+        self.fixed = fixed
+        self.num_features = num_features
+        self.token_per_path = token_per_path
+        if pathway_to_cls is not None:
+            if isinstance(pathway_to_cls, str):
+                pathway_to_cls = eval(pathway_to_cls)
+            self.pathway_to_cls = tf.constant(pathway_to_cls)
+        if class_token and pathway_to_cls is None:
+            if not token_per_path:
+                self.pathway_to_cls = tf.zeros(n, dtype=tf.int32)
+            else:
+                self.pathway_to_cls = tf.range(n, dtype=tf.int32)
+        self.num_features_per_path = int(num_features * d)
+        self.intersection = intersection
+        self.class_token = class_token
+        self.shift = (tf.reduce_max(self.pathway_to_cls) + 1) if class_token else 0
+        self.indices = None
+        if fixed:
+            set_seed(self.seed)
+            self.get_indices()
+
+    def get_config(self):
+        return dict(**super().get_config(), n=self.n, seed=self.seed, fixed=self.fixed,
+                    num_features=self.num_features, intersection=self.intersection,
+                    shift=self.shift, class_token=self.class_token, pathway_to_cls=self.pathway_to_cls)
+
+    def get_indices(self):
+        if self.indices is None:
+            if self.intersection:
+                indices = tf.stack(
+                    [tf.random.shuffle(tf.range(self.shift, self.num_patches + self.shift))[:self.num_features_per_path]
+                     for _ in range(self.n)],
+                    axis=-1)
+            else:
+                indices = tf.reshape(
+                    tf.random.shuffle(tf.range(self.shift,
+                                               self.num_patches + self.shift))[:self.num_features_per_path * self.n],
+                    (-1, self.n))
+
+            if self.fixed:
+                self.indices = indices
+        else:
+            indices = self.indices
+
+        # everyone gets the class token
+        if self.class_token:
+            indices = tf.concat([self.pathway_to_cls[None], indices], axis=0)
+        return indices
+
+    def call(self, inputs, training=False):
+        if not training:
+            set_seed(self.seed)
+
+        indices = self.get_indices()
+
+        return tf.gather(inputs, indices, axis=-2, batch_dims=0)
+
+
+class BasicRNNLayer(tf.keras.layers.Layer):
+    def __init__(self, width=32, dropout=0.1, regularization=None, name='basic_rnn_layer'):
+        super(BasicRNNLayer, self).__init__(name=name)
+        self.recurrent_connections = tf.keras.layers.Dense(width, activation=None, use_bias=False, name=f"{name}_recurrent", kernel_regularization=regularization)
+        self.input_projection = tf.keras.layers.Dense(width, activation=None, use_bias=True, name=f"{name}_proj", kernel_regulzarization=regularization)
+        self.dropout = tf.keras.layers.Dropout(dropout, name=f"{name}_dropout") if dropout else None
+        self.activation = tf.keras.layers.ReLU(name=f"{name}_relu")
+        self.internal_state_size = width
+
+    def call(self, inputs, state):
+        external_input = self.input_projection(inputs)
+        internal = self.recurrent_connections(self.dropout(state))
+        output = self.activation(internal + external_input)
+        return output
--- vision/model/layers.py
+++ vision/model/layers.py
@@ -1,71 +1,13 @@
+from utils.model.layers import SplitPathways
 from vision.utils.tf_utils import serialize
 
 
-
 @serialize
-class SplitPathwaysVision(tf_layers.Layer):
+class SplitPathwaysVision(SplitPathways):
     def __init__(self, num_patches, token_per_path=False, n=2, d=0.5, intersection=True, fixed=False,
                  seed=0, class_token=True, pathway_to_cls=None, **kwargs):
-        super(SplitPathwaysVision, self).__init__(**kwargs)
-        assert intersection or (n*int(num_patches * d)) <= num_patches
-        self.n = n
-        self.seed = seed
-        self.fixed = fixed
+        super(SplitPathwaysVision, self).__init__(num_features=num_patches, token_per_path=token_per_path, n=n, d=d,
+                                                  intersection=intersection, fixed=fixed, seed=seed,
+                                                  class_token=class_token, pathway_to_cls=pathway_to_cls, **kwargs)
         self.num_patches = num_patches
-        self.token_per_path = token_per_path
-        if pathway_to_cls is not None:
-            if isinstance(pathway_to_cls, str):
-                pathway_to_cls = eval(pathway_to_cls)
-            self.pathway_to_cls = tf.constant(pathway_to_cls)
-        if class_token and pathway_to_cls is None:
-            if not token_per_path:
-                self.pathway_to_cls = tf.zeros(n, dtype=tf.int32)
-            else:
-                self.pathway_to_cls = tf.range(n, dtype=tf.int32)
-        self.num_patches_per_path = int(num_patches * d)
-        self.intersection = intersection
-        self.class_token = class_token
-        self.shift = (tf.reduce_max(self.pathway_to_cls) + 1) if class_token else 0
-        self.indices = None
-        if fixed:
-            set_seed(self.seed)
-            self.get_indices()
-
-    def get_config(self):
-        return dict(**super().get_config(), n=self.n, seed=self.seed, fixed=self.fixed,
-                    num_patches=self.num_patches, intersection=self.intersection,
-                    shift=self.shift, class_token=self.class_token, pathway_to_cls=self.pathway_to_cls)
-
-    def get_indices(self):
-        if self.indices is None:
-            if self.intersection:
-                indices = tf.stack(
-                    [tf.random.shuffle(tf.range(self.shift, self.num_patches + self.shift))[:self.num_patches_per_path]
-                     for _ in range(self.n)],
-                    axis=-1)
-            else:
-                indices = tf.reshape(
-                    tf.random.shuffle(tf.range(self.shift,
-                                               self.num_patches + self.shift))[:self.num_patches_per_path * self.n],
-                    (-1, self.n))
-
-            if self.fixed:
-                self.indices = indices
-        else:
-            indices = self.indices
-
-        # everyone gets the class token
-        if self.class_token:
-            # cls_tokens_to_add = tf.range(self.n, dtype=indices.dtype)[None] if self.token_per_path else tf.zeros(
-            #     (1, self.n),
-            #     dtype=indices.dtype)
-            indices = tf.concat([self.pathway_to_cls[None], indices], axis=0)
-        return indices
-
-    def call(self, inputs, training=False):
-        if not training:
-            set_seed(self.seed)
-
-        indices = self.get_indices()
-
-        return tf.gather(inputs, indices, axis=-2, batch_dims=0)
+        self.num_patches_per_path = self.num_features_per_path
