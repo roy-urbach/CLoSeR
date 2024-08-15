28d90763706b010399973ad634ce638a6d19ac86
royur <roy.urbach@weizmann.ac.il>
2024-08-15 18:30:05 +0300

    - added BasicRNN

--- utils/model/encoders.py
+++ utils/model/encoders.py
@@ -1,5 +1,6 @@
 from utils.model.layers import *
 from tensorflow.keras import layers as tf_layers
+import tensorflow as tf
 
 from vision.utils.tf_utils import serialize
 
@@ -32,3 +33,23 @@ class MLPEncoder(MLP):
 
     def call(self, inputs, **kwargs):
         return self.out_layer(super().call(self.flatten(inputs), **kwargs))
+
+
+class BasicRNN:
+    def __init__(self, out_dim=None, name='rnn', **kwargs):
+        self.rnn = BasicRNNLayer(name=name + "_internal", **kwargs)
+        self.out_proj = tf.keras.layer.Dense(out_dim, name=name + "_out") if out_dim is not None else None
+
+    def __call__(self, inputs):
+        # inputs shape =  (B, T, N)
+        internal_state = tf.constant(tf.zeros((tf.shape(inputs)[0], self.rnn.internal_state_size), dtype=inputs.dtype))
+        states = []
+        for t in range(tf.shape(inputs)[1]):
+            internal_state = self.rnn(inputs[..., t, :], internal_state)
+            states.append(internal_state)
+        states = tf.stack(states, axis=1)  # (B, T, INTERNAL_DIM)
+        if self.out_proj is not None:
+            out = self.out_proj(states)    # (B, T, OUTDIM)
+        else:
+            out = states
+        return out
