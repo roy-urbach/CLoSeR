- made continuous loss MAE - Made an enum Labels to make things easier - fixed split_pathway wrong shape assumptions

neuronal/model/model.py
@@ -1,9 +1,14 @@
 import tensorflow as tf
 from tf.keras import layers
 
+from neuronal.utils.data import Labels, CATEGORICAL
 from utils.model.layers import SplitPathways
+from utils.model.losses import SparseCategoricalCrossEntropyByKey, LateralPredictiveLoss, NullLoss, MeanAbsoluteErrorByKeyLoss
+from utils.model.metrics import SparseCategoricalAccuracyByKey, MeanAbsoluteErrorByKeyMetric
+from utils.model.temporal_losses import CrossPathwayTemporalContrastiveLoss
 from utils.utils import get_class
 from vision.utils.tf_utils import set_seed, serialize
+import numpy as np
 
 
 def get_neuronal_data_augmentation(inputs, **kwargs):
@@ -14,19 +19,19 @@ def get_neuronal_data_augmentation(inputs, **kwargs):
 class SplitPathwaysNeuronal(SplitPathways):
     """
     Call
-    :param inputs: (B, T, N)
-    :return: (B, T, K, d*N)
+    :param inputs: (B, S, T)
+    :return: (B, d*S, N, DIM)
     """
 
     def __init__(self, num_units, n=2, d=0.5, intersection=True, fixed=False, seed=0, **kwargs):
-        super(SplitPathwaysNeuronal, self).__init__(num_channels=num_units, n=n, d=d, intersection=intersection,
+        super(SplitPathwaysNeuronal, self).__init__(num_signals=num_units, n=n, d=d, intersection=intersection,
                                                     fixed=fixed, seed=seed, class_token=False, **kwargs)
         self.num_units = num_units
 
 
 def create_neuronal_model(name='neuronal_model', frames=9, bins_per_frame=1, units=100,
                           classifier=False, l2=False, kernel_regularizer=None,
-                          projection_dim=64, encoder='BasicRNN', encoder_per_path=False,
+                          encoder='BasicRNN', encoder_per_path=False,
                           pathway_classification=True, pathway_classification_allpaths=False,
                           ensemble_classification=False, classifier_pathways=True,
                           augmentation_kwargs={}, encoder_kwargs={}, pathways_kwargs={}):
@@ -41,8 +46,8 @@ def create_neuronal_model(name='neuronal_model', frames=9, bins_per_frame=1, uni
     if classifier and not classifier_pathways:
         pathways = [augmented]
     else:
-        pathways = SplitPathwaysNeuronal(units, name=name + '_pathways', **pathways_kwargs)(augmented)
-        pathways = [tf.squeeze(path, axis=-2) for path in tf.split(pathways, pathways.shape[-2], axis=-2)]
+        pathways = SplitPathwaysNeuronal(units, name=name + '_pathways', **pathways_kwargs)(augmented)  # (B, d*S, N, T)
+        pathways = [tf.squeeze(path, axis=-2) for path in tf.split(pathways, pathways.shape[-2], axis=-2)] # List[(B, d*S, T)]
 
     import utils.model.encoders
     Encoder = get_class(encoder, utils.model.encoders)
@@ -53,29 +58,95 @@ def create_neuronal_model(name='neuronal_model', frames=9, bins_per_frame=1, uni
 
     embedding = tf.keras.layers.Concatenate(name=name + '_embedding', axis=-1)([encoder(pathway)[..., None]
                                                                                 for encoder, pathway in zip(encoders, pathways)])
-    # (B, T, C, P)
+    # (B, T, DIM, P)
 
     outputs = [embedding]
 
     last_step_embedding = embedding[:, -1]
 
-    # # classification heads, with stop_grad unless classifier=True
-    # if pathway_classification:
-    #     # Only for the first pathway
-    #     embedding_for_classification = last_step_embedding if classifier else tf.stop_gradient(last_step_embedding)
-    #     pathways_to_classify = list(range(len(pathways))) if pathway_classification_allpaths else [0]
-    #     for path in pathways_to_classify:
-    #         cur_embd = embedding_for_classification[..., path]
-    #         cur_name = name + '_logits' + (str(path) if pathway_classification_allpaths else '')
-    #         pathway_logits = layers.Dense(num_classes, activation=None,
-    #                                       kernel_regularizer=kernel_regularizer, name=cur_name)(cur_embd)
-    #         outputs.append(pathway_logits)
-    # if ensemble_classification:
-    #     outputs.append(layers.Dense(num_classes, activation=None, kernel_regularizer=kernel_regularizer, name=name + '_ensemble_logits')(
-    #         tf.reshape(embedding if classifier else tf.stop_gradient(embedding), (-1, np.multiply.reduce(embedding.shape[1:])))))
+    # classification heads, with stop_grad unless classifier=True
+    if pathway_classification:
+        # Only for the first pathway
+        embedding_for_classification = last_step_embedding if classifier else tf.stop_gradient(last_step_embedding)
+        if bins_per_frame > 1:
+            embedding_for_classification = embedding_for_classification[:, bins_per_frame - 1::bins_per_frame]
+
+        pathways_to_classify = list(range(len(pathways))) if pathway_classification_allpaths else [0]
+        for path in pathways_to_classify:
+            cur_embd = embedding_for_classification[..., path]
+            for label in Labels:
+                cur_name = name + '_logits' + (str(path) if pathway_classification_allpaths else '') + f'_{label.value.name}'
+                pathway_logits = layers.Dense(label.value.dimension, activation=None,
+                                              kernel_regularizer=kernel_regularizer, name=cur_name)(cur_embd)
+                outputs.append(pathway_logits)
+    if ensemble_classification:
+        for label in Labels:
+            outputs.append(layers.Dense(label.value.dimension, activation=None,
+                                        kernel_regularizer=kernel_regularizer, name=name + f'_ensemble_logits_{label.value.name}')(
+                tf.reshape(embedding if classifier else tf.stop_gradient(embedding), (-1, np.multiply.reduce(embedding.shape[1:])))))
 
     # Create the Keras model.
     model = tf.keras.Model(inputs=inputs, outputs=outputs, name=name)
     return model
 
 
+def compile_model(model, loss=CrossPathwayTemporalContrastiveLoss, loss_kwargs={},
+                  optimizer_cls=tf.optimizers.legacy.Nadam if tf.__version__ == '2.12.0' else tf.optimizers.Nadam,
+                  optimizer_kwargs={}, classifier=False, pathway_classification=True,
+                  ensemble_classification=False, pathway_classification_allpaths=False,
+                  metrics_kwargs={}, **kwargs):
+    if kwargs:
+        print(f"WARNING: compile_model got spare kwargs that won't be used: {kwargs}")
+
+    if "optimizer" in optimizer_kwargs:
+        optimizer_cls_name = optimizer_kwargs.pop("optimizer")
+        try:
+            optimizer_cls = eval(optimizer_cls_name)
+        except Exception as err:
+            print(f"Tried to eval {optimizer_cls_name}, get error:", err)
+            try:
+                optimizer_cls = tf.keras.optimizers.getattr(optimizer_cls_name)
+            except Exception as err:
+                print(f"Tried to getattr tf.keras.optimizers.{optimizer_cls_name}, get error:", err)
+    print(f"using optimizer {optimizer_cls}")
+    optimizer = optimizer_cls(**{k: eval(v) if isinstance(v, str) and v.startswith("tf.") else v
+                                 for k, v in optimizer_kwargs.items()})
+    serialize(optimizer.__class__, 'Custom')
+
+    losses = {}
+    metrics = {}
+    if classifier:
+        losses[model.name + '_embedding'] = NullLoss()
+    else:
+        losses[model.name + '_embedding'] = loss(**loss_kwargs)
+
+    if (model.name + "_predembd") in [l.name for l in model.layers]:
+        losses[model.name + "_predembd"] = LateralPredictiveLoss(graph=model.get_layer(model.name + "_predembd").pred_graph)
+
+    if metrics_kwargs:
+        import utils.model.metrics as metrics_file
+        metrics[model.name + "_embedding"] = get_class(metrics_kwargs['name'], metrics_file)(metrics_kwargs.get('kwargs', {}))
+
+    label_class_loss = {
+        label.value.name: SparseCategoricalCrossEntropyByKey if label.value.kind == CATEGORICAL else MeanAbsoluteErrorByKeyLoss
+        for label in Labels}
+    label_class_metric = {
+        label.value.name: SparseCategoricalAccuracyByKey if label.value.kind == CATEGORICAL else MeanAbsoluteErrorByKeyMetric
+        for label in Labels}
+
+    if pathway_classification:
+        if pathway_classification_allpaths:
+            for path in range(model.get_layer(model.name + "_pathways").n):
+                for label in Labels:
+                    losses[model.name + f'_logits{path}_{label.value.name}'] = label_class_loss[label.value.name](label.value.name, from_logits=True)
+                    metrics[model.name + f'_logits{path}_{label.value.name}'] = label_class_metric[label.value.name](label.value.name)
+        else:
+            for label in Labels:
+                losses[model.name + f'_logits_{label.value.name}'] = label_class_loss[label.value.name](label.value.name, from_logits=True)
+                metrics[model.name + f'_logits_{label.value.name}'] = label_class_metric[label.value.name](label.value.name)
+
+    if ensemble_classification:
+        for label in Labels:
+            losses[model.name + f'_ensemble_logits_{label.value.name}'] = label_class_loss[label.value.name](label.value.name, from_logits=True)
+            metrics[model.name + f'_ensemble_logits_{label.value.name}'] = label_class_metric[label.value.name](label.value.name, name="accuracy")
+    model.compile(optimizer=optimizer, loss=losses, metrics=metrics)

utils/model/encoders.py
@@ -41,11 +41,11 @@ class BasicRNN:
         self.out_proj = tf.keras.layer.Dense(out_dim, name=name + "_out") if out_dim is not None else None
 
     def __call__(self, inputs):
-        # inputs shape =  (B, T, N)
+        # inputs shape =  (B, N, T)
         internal_state = tf.constant(tf.zeros((tf.shape(inputs)[0], self.rnn.internal_state_size), dtype=inputs.dtype))
         states = []
-        for t in range(tf.shape(inputs)[1]):
-            internal_state = self.rnn(inputs[..., t, :], internal_state)
+        for t in range(tf.shape(inputs)[-1]):
+            internal_state = self.rnn(inputs[..., t], internal_state)
             states.append(internal_state)
         states = tf.stack(states, axis=1)  # (B, T, INTERNAL_DIM)
         if self.out_proj is not None:

utils/model/layers.py
@@ -300,14 +300,17 @@ class ConvNet:
 
 @serialize
 class SplitPathways(tf.keras.layers.Layer):
-    def __init__(self, num_features, token_per_path=False, n=2, d=0.5, intersection=True, fixed=False,
+    # Receives (B, ..., S, DIM)
+    # Outputs (B, ..., d*S, N, DIM)
+
+    def __init__(self, num_signals, token_per_path=False, n=2, d=0.5, intersection=True, fixed=False,
                  seed=0, class_token=True, pathway_to_cls=None, **kwargs):
         super(SplitPathways, self).__init__(**kwargs)
-        assert intersection or (n*int(num_features * d)) <= num_features
+        assert intersection or (n*int(num_signals * d)) <= num_signals
         self.n = n
         self.seed = seed
         self.fixed = fixed
-        self.num_features = num_features
+        self.num_signals = num_signals
         self.token_per_path = token_per_path
         if pathway_to_cls is not None:
             if isinstance(pathway_to_cls, str):
@@ -318,7 +321,7 @@ class SplitPathways(tf.keras.layers.Layer):
                 self.pathway_to_cls = tf.zeros(n, dtype=tf.int32)
             else:
                 self.pathway_to_cls = tf.range(n, dtype=tf.int32)
-        self.num_features_per_path = int(num_features * d)
+        self.num_signals_per_path = int(num_signals * d)
         self.intersection = intersection
         self.class_token = class_token
         self.shift = (tf.reduce_max(self.pathway_to_cls) + 1) if class_token else 0

utils/model/temporal_losses.py
@@ -1,9 +1,8 @@
 import tensorflow as tf
-from tensorflow.keras.losses import Loss
 
 
-class CrossPathwayTemporalContrastiveLoss(Loss):
-    def __init__(self, *args, a=None, contrast_t=3, start_t=0, temperature=10, **kwargs):
+class CrossPathwayTemporalContrastiveLoss(tf.keras.losses.Loss):
+    def __init__(self, *args, a=None, contrast_t=1, start_t=0, temperature=10, **kwargs):
         super(CrossPathwayTemporalContrastiveLoss, self).__init__(*args, **kwargs)
         self.temperature = temperature
         self.a = a