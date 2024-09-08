b04efb0a41c2a289673dd7b90a824b55e63d3d16
royur <roy.urbach@weizmann.ac.il>
2024-09-08 10:07:46 +0300

    - added HistoryWithMetrics

--- utils/model/callbacks.py
+++ utils/model/callbacks.py
@@ -65,3 +65,15 @@ class SaveHistoryWithMetrics(SaveHistory):
                     logs[metric_name] = metric.numpy()
 
         super().on_epoch_end(epoch, logs=logs)
+
+
+class HistoryWithMetrics(tf.keras.callbacks.History):
+    def on_epoch_end(self, epoch, logs=None):
+        logs = logs or {}
+        for loss_name, loss in self.model.loss.items():
+            # Access the loss values from the model
+            if hasattr(self.model.loss, 'get_metrics'):
+                for metric_name, metric in loss.get_metrics():
+                    logs[metric_name] = metric.numpy()
+
+        super().on_epoch_end(epoch, logs=logs)
