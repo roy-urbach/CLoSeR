ceb770e3e3a99ac2ffb89b34cfad320122b9a8b8
royur <roy.urbach@weizmann.ac.il>
2024-12-03 18:53:17 +0200

    - digitize better

--- neuronal/utils/data.py
+++ neuronal/utils/data.py
@@ -604,7 +604,7 @@ class PlaceCellsDS(TemporalData):
             self.trajectory = trajectory
             if Labels.LOCATION1D.value.is_categorical():
                 min_, max_ = trajectory.min(), trajectory.max()
-                trajectory = np.digitize(trajectory, np.linspace(min_, max_, Labels.LOCATION1D.value.dimension+1)[:-1])
+                trajectory = np.digitize(trajectory, np.linspace(min_, max_, Labels.LOCATION1D.value.dimension+1)[1:])
             elif self.normalize_traj:
                 trajectory = (trajectory - trajectory.mean()) / trajectory.std(ddof=1)
             self.y_samples = {Labels.LOCATION1D.value.name: trajectory[..., None]}
