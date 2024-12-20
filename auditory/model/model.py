81327beee50f55b465decef576d5b2620db3e49a
royur <roy.urbach@weizmann.ac.il>
2024-12-20 12:22:28 +0200

    - added labels eval in auditory

--- auditory/model/model.py
+++ auditory/model/model.py
@@ -5,9 +5,11 @@ from utils.modules import Modules
 
 
 def create_model(input_shape, name='auditory_model', encoder='TimeAgnosticMLP',
-                 labels=(AuditoryLabels.BIRD), module=Modules.AUDITORY, **kwargs):
+                 labels=(AuditoryLabels.BIRD, ), module=Modules.AUDITORY, **kwargs):
+    labels = [eval(label) if isinstance(label, str) else label for label in labels]
     return neur_model.create_model(input_shape, name=name, encoder=encoder, labels=labels, module=module, **kwargs)
 
 
-def compile_model(model, dataset, loss=neur_losses.LPL, labels=(AuditoryLabels.BIRD), **kwargs):
+def compile_model(model, dataset, loss=neur_losses.LPL, labels=(AuditoryLabels.BIRD, ), **kwargs):
+    labels = [eval(label) if isinstance(label, str) else label for label in labels]
     return neur_model.compile_model(model, dataset, loss=loss, labels=labels, **kwargs)
