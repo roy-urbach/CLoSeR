75136f5e005704baa37d1b9311b520de694ec0fa
royur <roy.urbach@weizmann.ac.il>
2024-12-31 12:19:05 +0200

    - added p_pink and p_white

--- utils/model/augmentations.py
+++ utils/model/augmentations.py
@@ -46,7 +46,7 @@ class MelSpectrogramAugmenter(tf.keras.layers.Layer):
         white_noise_factor: Factor to control the intensity of the white noise.
     """
 
-    def __init__(self, sr, f_min, f_max, pink_noise_factor=0.05, white_noise_factor=0.02, seed=None, name='spect_noise'):
+    def __init__(self, sr, f_min, f_max, pink_noise_factor=0.05, white_noise_factor=0.02, p_pink=0.5, p_white=0.5, name='spect_noise'):
         super().__init__(name=name)
         self.sr = sr
         self.n_mels = None
@@ -54,6 +54,8 @@ class MelSpectrogramAugmenter(tf.keras.layers.Layer):
         self.f_max = f_max
         self.pink_noise_factor = pink_noise_factor
         self.white_noise_factor = white_noise_factor
+        self.p_pink = p_pink
+        self.p_white = p_white
 
     def build(self, input_shape):
         actual_shape = input_shape[1:]
@@ -86,8 +88,11 @@ class MelSpectrogramAugmenter(tf.keras.layers.Layer):
 
         # self.zeros = self.add_weight(shape=actual_shape, initializer='zeros', trainable=False)
 
-    def get_noise(self, shape, dtype=None):
-        return tf.random.normal(shape, dtype=dtype, mean=0, stddev=1)
+    def get_noise(self, shape, p=1., dtype=None):
+        noise = tf.random.normal(shape, dtype=dtype, mean=0, stddev=1)
+        if p < 1.:
+            noise = tf.cast(tf.random.rand(shape[0]) <= p, dtype=noise.dtype) * noise
+        return noise
 
     def call(self, inputs, training=None):
         """
@@ -103,11 +108,11 @@ class MelSpectrogramAugmenter(tf.keras.layers.Layer):
             return inputs
 
         # Generate pink noise
-        noise = self.get_noise(tf.shape(inputs), dtype=inputs.dtype)
+        noise = self.get_noise(tf.shape(inputs), p=self.p_pink, dtype=inputs.dtype)
         pink_noise = noise * self.mel_pink_noise_psd[None]
 
         # Generate white noise
-        white_noise = self.get_noise(tf.shape(inputs), dtype=inputs.dtype)
+        white_noise = self.get_noise(tf.shape(inputs), p=self.p_white, dtype=inputs.dtype)
 
         # Add noise to the mel-spectrogram
         augmented_mel = inputs + self.pink_noise_factor * pink_noise + self.white_noise_factor * white_noise
