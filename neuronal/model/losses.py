d99416cf1de5512904225deae24e76ad5e9d303c
royur <roy.urbach@weizmann.ac.il>
2025-01-01 12:43:34 +0200

    - push_w multiplied by -, no division by 2

--- neuronal/model/losses.py
+++ neuronal/model/losses.py
@@ -914,8 +914,8 @@ class LPL(tf.keras.losses.Loss):
             pull_graph = (tf.random.uniform(shape=(num_pathways, num_pathways), maxval=1) < ppull) & ~tf.eye(num_pathways, dtype=tf.bool)
             if push_w:
                 push_graph = ~pull_graph & ~tf.eye(num_pathways, dtype=tf.bool)
-            self.pullpush_graph = [tf.cast(pull_graph, tf.float64) / num_pathways * (num_pathways - 1) * 2,
-                                   tf.cast(push_graph, tf.float64) / num_pathways * (num_pathways - 1) * 2 if push_w else None]
+            self.pullpush_graph = [tf.cast(pull_graph, tf.float64) / num_pathways * (num_pathways - 1),
+                                   tf.cast(push_graph, tf.float64) / num_pathways * (num_pathways - 1) if push_w else None]
 
     def pullpush(self, embd, pull_w=1, push_w=1, logpush=False, **kwargs):
         # (B, DIM, P)
@@ -935,7 +935,7 @@ class LPL(tf.keras.losses.Loss):
             else:
                 push_mse = mse
             push_loss = tf.tensordot(push_mse, tf.cast(self.pullpush_graph[1], dtype=embd.dtype), axes=[[0,1], [0,1]])
-            total_loss = total_loss + push_w * push_loss
+            total_loss = total_loss - push_w * push_loss
             self.monitor.update_monitor("push", push_loss)
 
         return total_loss
