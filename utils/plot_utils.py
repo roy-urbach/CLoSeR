99b37e7bee8ec8d938b50d43f0041c01697defe0
royur <roy.urbach@weizmann.ac.il>
2025-01-16 15:31:05 +0200

    - added scatter to violinplot

--- utils/plot_utils.py
+++ utils/plot_utils.py
@@ -52,7 +52,7 @@ def savefig(fn):
     print(f"saved figure as {fn}")
 
 
-def violinplot_with_CI(arr, x, c='C0', widths=0.5, bar=False, **kwargs):
+def violinplot_with_CI(arr, x, c='C0', widths=0.5, bar=False, scatter=False, **kwargs):
     if bar:
        plt.bar(x, arr.mean(), color=c, **kwargs)
     else:
@@ -64,6 +64,8 @@ def violinplot_with_CI(arr, x, c='C0', widths=0.5, bar=False, **kwargs):
     n = arr.size
     CI = np.abs(scipy.stats.t.ppf(0.025, n - 1) * std / np.sqrt(n))
     plt.errorbar([x], mean, yerr=CI, marker='o', capsize=10, c=c)
+    if scatter:
+        plt.scatter(x, arr, alpha=kwargs.get("alpha", 0.8), c=c)
 
 
 def multiviolin(arr, xshift=0, xs=None, fig=None, **kwargs):
