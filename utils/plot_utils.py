512157474461da8f44db22a22df12ef4cc90cdc9
royur <roy.urbach@weizmann.ac.il>
2025-01-30 18:08:09 +0200

    - fixed

--- utils/plot_utils.py
+++ utils/plot_utils.py
@@ -14,7 +14,7 @@ def basic_scatterplot(x, y, identity=True, fig=None, c='k', corr=False, t=False,
 
     mask = np.isnan(x) | np.isnan(y)
     x, y = x[~mask], y[~mask]
-    if isinstance(c, np.ndarray) and c.size == mask.size:
+    if isinstance(c, np.ndarray) and len(c) == mask.size:
         c = c[mask]
 
     x_ = np.log(x) if log_x else x
