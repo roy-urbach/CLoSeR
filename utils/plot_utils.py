21ded46d91c0271da11101019bb27d1bd31bcd2b
royur <roy.urbach@weizmann.ac.il>
2025-02-10 12:09:48 +0200

    - trying something weird

--- utils/plot_utils.py
+++ utils/plot_utils.py
@@ -45,6 +45,12 @@ class NamesAndColors(Enum):
     UNSUPERVISED = NameAndColor("CLoSeR", color='g')
     SUPERVISED = NameAndColor("supervised", color='r')
 
+    def __getattribute__(self, item):
+        if hasattr(self, item):
+            return super().__getattribute__(item)
+        else:
+            return getattr(self.value, item)
+
 
 
 def basic_scatterplot(x, y, identity=True, fig=None, c='k', corr=False, t=False,
