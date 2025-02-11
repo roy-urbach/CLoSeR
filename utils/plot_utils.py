8975dd205cd63c149f140017d0fbd7a063e52ae7
royur <roy.urbach@weizmann.ac.il>
2025-02-11 15:03:12 +0200

    - added SHARED

--- utils/plot_utils.py
+++ utils/plot_utils.py
@@ -54,6 +54,7 @@ class NamesAndColors(Enum):
     CHANCE = NameAndColor("chance level", color='k', linestyle=':')
     MASKED = NameAndColor("masked raw input", i=0)
     UNTRAINED = NameAndColor("untrained model", color='purple')
+    SHARED = NameAndColor("shared weights CLoSeR", color='yellow')
     FULL_INPUT = NameAndColor("full raw input", i=0)
     UNSUPERVISED = NameAndColor("CLoSeR", color='g')
     SUPERVISED = NameAndColor("supervised", i=3)
