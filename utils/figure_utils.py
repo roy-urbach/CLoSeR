0314e85028a5908996604861901b54cc669ac0b5
royur <roy.urbach@weizmann.ac.il>
2025-02-12 16:48:35 +0200

    - fixing

--- utils/figure_utils.py
+++ utils/figure_utils.py
@@ -80,11 +80,14 @@ def plot_history(model_regex, module, window=10, name_to_name=lambda m: m, log=T
         plt.title((model_regex if title is None else title) + " " + k)
         for i, (model_name, value) in enumerate(v.items()):
             if plot_train:
-                plt.plot(*smooth(value, window), label=model_name,
-                         c=f"C{i}" if name_to_c is None else name_to_c(orig_names[i]))
+                if value:
+                    plt.plot(*smooth(value, window), label=model_name,
+                             c=f"C{i}" if name_to_c is None else name_to_c(orig_names[i]))
             if plot_val:
-                plt.plot(*smooth(histories["val_"+k][model_name], window), label=model_name if not plot_train else None,
-                         c=f"C{i}" if name_to_c is None else name_to_c(orig_names[i]), linestyle=':' if plot_train else '-')
+                val = histories["val_"+k][model_name]
+                if val:
+                    plt.plot(*smooth(val, window), label=model_name if not plot_train else None,
+                             c=f"C{i}" if name_to_c is None else name_to_c(orig_names[i]), linestyle=':' if plot_train else '-')
         if log and any([key in k for key in log_keys]):
             plt.yscale("log")
         if legend:
