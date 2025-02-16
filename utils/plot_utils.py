25ed0cca4d98813954d4e9b4f24dde2a26a05316
royur <roy.urbach@weizmann.ac.il>
2025-02-16 21:07:26 +0200

    - lightgray

--- utils/plot_utils.py
+++ utils/plot_utils.py
@@ -58,7 +58,7 @@ class NameAndColor:
 class NamesAndColors(Enum):
     CHANCE = NameAndColor("chance level", color='k', linestyle=':')
     MASKED = NameAndColor("masked raw input", i=0)
-    UNTRAINED = NameAndColor("untrained model", color='gray')
+    UNTRAINED = NameAndColor("untrained model", color='lightgray')
     SHARED = NameAndColor("shared weights CLoSeR", i=5)
     FULL_INPUT = NameAndColor("full raw input", i=0)
     UNSUPERVISED = NameAndColor("CLoSeR", color='g')
